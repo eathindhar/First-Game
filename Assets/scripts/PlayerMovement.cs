7aa05a83e893bc6806f097256cf57d48dc7769ad
eathindhar <cldveathi@gmail.com>
2017-12-05 21:11:39 +0530

    Added the files

---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float forwardforce = 1000f;
+    public float sidewaysforce = 5000f;
+
+	void Start () {
+        Debug.Log("Game Started");
+        //rb.AddForce(0, 200, 100);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        rb.AddForce(0, 0, forwardforce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysforce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysforce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
---
+++ Assets/scripts/Score.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class Score : MonoBehaviour {
+    public Transform player;
+    public Text scoretext;
+    private void Update()
+    {
+        //Debug.Log(player.position.z);
+        scoretext.text = player.position.z.ToString("0");
+
+    }
+}
